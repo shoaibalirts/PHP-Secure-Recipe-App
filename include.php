196b073b9809d830f1fb603f138be617de9c465c
Shoaibalirts <shoaibali.rts@gmail.com>
2024-12-25 22:39:04 +0100

    completed project after using associavtive array to make the option only the available array keys and values, and lastly used isset for associative array

--- include.php
+++ include.php
@@ -1,34 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./simple.css" />
     <title>Document</title>
 </head>
+
 <body>
-<?php
-function e($value) {
-    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-}
+    <?php
+    function e($value)
+    {
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
 
-?>
+    ?>
 
-<form method="GET" action="include.php">
-    <select name="page">
-        <option value="">Please select a recipe</option>
-        <option value="citrus_salmon" <?php if (!empty($_GET['page']) && $_GET['page'] == 'citrus_salmon') echo 'selected'; ?>>Citrus Symphony Salmon</option>
-        <option value="mediterranian_pasta" <?php if (!empty($_GET['page']) && $_GET['page'] == 'mediterranian_pasta') echo 'selected'; ?>>Mediterranean Marvel Pasta</option>
-        <option value="sunset_risotto" <?php if (!empty($_GET['page']) && $_GET['page'] == 'sunset_risotto') echo 'selected'; ?>>Sunset Risotto</option>
-        <option value="tropical_tacos" <?php if (!empty($_GET['page']) && $_GET['page'] == 'tropical_tacos') echo 'selected'; ?>>Tropical Tango Tacos</option>
-    </select>
-    <input type="submit" value="Submit!" />
-</form>
+    <?php
+    $pages = [
+        'citrus_salmon' => 'citrus salmon',
+        'mediterranian_pasta' => 'mediterranian pasta',
+        'sunset_risotto' => 'sunset risotto',
+        'tropical_tacos' => 'tropical tacos',
+    ];
+    ?>
 
-<?php
-    echo file_get_contents("pages/{$_GET['page']}.html");
-?>
+    <form method="GET" action="include.php">
+        <select name="page">
+            <option value="">Please select a recipe</option>
+            <?php foreach ($pages as $pageKey => $pageValue) { ?>
+                <option value=<?php echo e($pageKey); ?> <?php if (!empty($_GET['page']) && $_GET['page'] == $pageKey) echo 'selected'; ?>><?php echo e($pageValue) ?></option>
+            <?php } ?>
+
+
+        </select>
+        <input type="submit" value="Submit!" />
+    </form>
+
+
+    <?php
+    if (!empty($_GET['page'])) {
+        $page = $_GET['page'];
+        if (isset($pages[$page])) {
+        echo file_get_contents("pages/{$page}.html");
+        }
+    }
+    ?>
+
 
 
 </body>
+
 </html>
